33cc067437a34673b8c833698680a1a5048dd652
timeshp <timeshp@gmail.com>
2014-10-05 00:05:39 -0400

    updated parser with more API methods

--- src/parser/Parser.java
+++ src/parser/Parser.java
@@ -1,5 +1,7 @@
 package parser;
 
+import javafx.scene.input.KeyCode;
+
 public class Parser {
     /**
      * takes in a string and parses it by puts all commands into a graph 
@@ -10,6 +12,26 @@ public class Parser {
    public String parse(String string){
        return string;
    }
-   
-   
+   /**
+    * If user needs to give mouseinput to parser 
+    * @param x
+    * @param y
+    */
+   public void onClick(double x, double y){
+       
+   }
+   /**
+    * if user presses a key to give parser info
+    * @param key
+    */
+   public void onKeyPress(KeyCode key){
+       
+   }
+   /**
+    * Takes a string to change parser language
+    * @param language
+    */
+   public void changeLanguage(String language){
+       
+   }
 }
