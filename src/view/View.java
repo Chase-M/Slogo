81763d0aa429b72f0b3c57626ac08870cffa4f71
David Zhang <David@davids-mbp-110.wireless.duke.local>
2014-10-05 00:06:46 -0400

    added to View

--- src/view/View.java
+++ src/view/View.java
@@ -1,13 +1,45 @@
 package view;
 
+import java.util.List;
+
+import actor.Info;
+
 public class View {
+
+	
+	public View(){
+		
+	}
 	
 	
 	/**
-	 * Manipulates the turtle and draws lines
+	 * passes list of turtle info to be updated in the GUI
+	 * @param list: contains information on the actor
 	 */
+	public void updateActors(List<Info> list){
+		
+	}
 	
-	public View(){
+	/**
+	 * updates the background color using information
+	 * @param info: background information
+	 */
+	public void updateStage(Info info){
+		
+	}
+	
+	/**
+	 * displays the error message
+	 * @param String: error message
+	 */
+	public void displayError(String str){
+		
+	}
+	
+	/**
+	 * clears and resets the display
+	 */
+	public void clearScreen(){
 		
 	}
 
