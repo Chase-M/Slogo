c7b7255e9a30dd9aa99dc1b56da64c7205ce5cc3
Chase Malik <chase.malik@duke.edu>
2014-10-05 00:28:49 -0400

    updated command constructor

--- src/command/Command.java
+++ src/command/Command.java
@@ -6,6 +6,14 @@ import parser.Node;
 import actor.Actor;
 
 public abstract class Command {
+	/**
+	 * Creates a command that will act on the list of actors given
+	 * @param actors
+	 */
+	public Command(List<Actor> actors){
+		
+	}
+	
 	
 	/**
 	 * Returns the number of inputs for the specific command
@@ -20,9 +28,8 @@ public abstract class Command {
 	 * and returns the appropriate integer value
 	 * 
 	 * @param inputs List of inputs for the specific command
-	 * @param actors List of actors to be acted on
 	 * @return appropriate int based on the type of command
 	 */
-	public abstract int execute(List<Node> inputs, List<Actor> actors);
+	public abstract int execute(List<Node> inputs);
 	
 }
