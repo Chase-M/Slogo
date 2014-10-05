cab5681ef3bc0e79bf1014c90b343c349dbf0461
Chase Malik <chase.malik@duke.edu>
2014-10-04 23:41:40 -0400

    Added Test + necessary subclasses for test

---
+++ src/command/IntCommand.java
@@ -0,0 +1,20 @@
+package command;
+
+import java.util.List;
+
+import parser.Node;
+import actor.Actor;
+
+public class IntCommand extends Command {
+
+	public IntCommand(int i){
+		
+	}
+	
+	@Override
+	public int execute(List<Node> inputs, List<Actor> actors) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
---
+++ src/command/SumCommand.java
@@ -0,0 +1,16 @@
+package command;
+
+import java.util.List;
+
+import parser.Node;
+import actor.Actor;
+
+public class SumCommand extends Command {
+
+	@Override
+	public int execute(List<Node> inputs, List<Actor> actors) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
