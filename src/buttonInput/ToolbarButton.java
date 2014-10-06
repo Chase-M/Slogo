6fe65053d143b25dfef2497191f0d8b7779968d5
Kevin Button <klb52@duke.edu>
2014-10-05 23:18:45 -0400

    Added border pane to GUI

--- src/buttonInput/ToolbarButton.java
+++ src/buttonInput/ToolbarButton.java
@@ -1,12 +1,25 @@
 package buttonInput;
 
-public abstract class ToolbarButton extends ButtonFactory{
+import features.Feature;
+import javafx.scene.control.MenuButton;
+
+public class ToolbarButton extends MenuButton implements Feature{
 	
+	private String myText;
 	
 	/**
 	 * Initializes the sub-methods to create a tool bar for in-game options
 	 */
 	public ToolbarButton(){
+	//	MenuButton button = new ToolbarButton(s);
+	//	return button;
+		super();
+		
+	}
+
+	@Override
+	public void update() {
+		// TODO Auto-generated method stub
 		
 	}
 	
--- src/mainApplication/GUI.java
+++ src/mainApplication/GUI.java
@@ -1,16 +1,45 @@
+
+
 package mainApplication;
 
+import java.util.ResourceBundle;
+
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.stage.Stage;
 import parser.Parser;
 import textInput.TextCommand;
 
-public class GUI {
+public class GUI extends Application {
 	
 	
 	/**
 	 * Activates the necessary components for GUI
 	 */
+
+	private Stage myStage;
+	private Group myRoot;
+	private ResourceBundle myNumberResources;
+	private int mySceneHeight;
+	private int mySceneWidth;
 	
 	public GUI(){
+		myStage = new Stage();
+		myRoot = new Group();
+		Scene s = new Scene(myRoot, 50, 50, Color.WHITE);
+		 
+		
+		
+		
+		
 		
 		/*
 		TextCommand exampleCommand = new TextCommand();
@@ -18,7 +47,61 @@ public class GUI {
 		Parser myParser = new Parser();
 		String returnCommand = myParser.parse(command);
 		*/
+	
+	}
+	public static void main(String[] args){
+	//	Stage s = new Stage();
+	//	start(s);
+		launch(args);
+	}
+	
+	
+	private Scene initiate(Group root){
+		myNumberResources = ResourceBundle.getBundle("resources/constants/numbers");
+		mySceneHeight = Integer.parseInt(myNumberResources.getString("Scene_height"));
+		mySceneWidth = Integer.parseInt(myNumberResources.getString("Scene_width"));
+		return new Scene(myRoot, mySceneHeight, mySceneWidth, Color.GREY);
+	}
+	
+	private BorderPane setBorderPane(Group g){
+		BorderPane pane = new BorderPane();
 		
+		HBox hbox = addHBox();
+		hbox.setStyle("-fx-background-color: #336699;");
+		hbox.getChildren().add(new Label("Name:"));
+		pane.setBottom(hbox);
+		g.getChildren().add(pane);
+		return pane;
+	}
+	
+	
+	public HBox addHBox() {
+	    HBox hbox = new HBox();
+	    hbox.setPrefWidth(mySceneWidth);
+	  //  hbox.setPrefHeight();
+	    hbox.setPadding(new Insets(15, 12, 15, 12));
+	    hbox.setSpacing(10);
+	    hbox.setStyle("-fx-background-color: #336699;");
+
+	    Button buttonCurrent = new Button("Current");
+	    buttonCurrent.setPrefSize(100, 20);
+
+	    Button buttonProjected = new Button("Projected");
+	    buttonProjected.setPrefSize(100, 20);
+	    hbox.getChildren().addAll(buttonCurrent, buttonProjected);
+
+	    return hbox;
 	}
 
-}
+	@Override
+	public void start(Stage stage) throws Exception {
+		myStage = new Stage();
+		myRoot = new Group();
+		Scene s = initiate(myRoot);
+		BorderPane pane = setBorderPane(myRoot);
+		myStage.setScene(s);
+		myStage.show();
+		
+		
+	}
+	}
