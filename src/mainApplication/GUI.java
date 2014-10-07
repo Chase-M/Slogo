e346da148fcf324fcadf91c54c9a53baf759372c
Kevin Button <klb52@duke.edu>
2014-10-06 22:59:14 -0400

    Added flowpane and scroll pane

--- src/mainApplication/GUI.java
+++ src/mainApplication/GUI.java
@@ -6,13 +6,18 @@ import java.util.ResourceBundle;
 
 import buttonInput.Toolbar;
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.geometry.Insets;
+import javafx.geometry.Orientation;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextArea;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
@@ -21,6 +26,8 @@ import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
 import parser.Parser;
 import textInput.TextCommand;
+import textOutput.ConsoleHistory;
+//import javafx.geometry.Orientation;
 
 public class GUI extends Application {
 	
@@ -34,6 +41,11 @@ public class GUI extends Application {
 	private ResourceBundle myNumberResources;
 	private int mySceneHeight;
 	private int mySceneWidth;
+	private ConsoleHistory myConsoleHistory;
+	private VBox vbox2;
+	private ScrollPane myScrollPane;
+//	public static final Orientation vert = VERTICAL;
+	private FlowPane myFlowPane;
 	
 	public GUI(){
 		myStage = new Stage();
@@ -64,7 +76,7 @@ public class GUI extends Application {
 		myNumberResources = ResourceBundle.getBundle("resources/constants/numbers");
 		mySceneHeight = Integer.parseInt(myNumberResources.getString("Scene_height"));
 		mySceneWidth = Integer.parseInt(myNumberResources.getString("Scene_width"));
-		return new Scene(myRoot, mySceneHeight, mySceneWidth, Color.GREY);
+		return new Scene(myRoot, mySceneWidth, mySceneHeight, Color.GREY);
 	}
 	
 	private BorderPane setBorderPane(Group g){
@@ -85,18 +97,34 @@ public class GUI extends Application {
 	  //  hbox.setPrefHeight();
 	    hbox.setPadding(new Insets(15, 12, 15, 12));
 	    hbox.setSpacing(10);
-	    hbox.setStyle("-fx-background-color: #336699;");
+	   // hbox.setStyle("-fx-background-color: #336699;");
 
 	    
-	    TextArea myCommand = new TextArea();
-	    myCommand.setLayoutY(100);
+	    final TextArea myCommand = new TextArea();
+	    //myCommand.setLayoutY(50);
+	    myCommand.setPrefRowCount(3);
 	    hbox.getChildren().add(myCommand);
 	  //  Button buttonCurrent = new Button("Current");
 	   // buttonCurrent.setPrefSize(100, 20);
 
-//	    Button buttonProjected = new Button("Projected");
-	//    buttonProjected.setPrefSize(100, 20);
-	  //  hbox.getChildren().addAll(buttonCurrent, buttonProjected);
+	   Button buttonProjected = new Button("Run");
+	   buttonProjected.setPrefSize(100, 20);
+	   buttonProjected.setOnAction(new EventHandler<ActionEvent>() {
+		    @Override public void handle(ActionEvent e) {
+		        System.out.println(myCommand.getText());
+		       // myCommand.clear();
+		        myConsoleHistory.update(myCommand.getText());
+		        myFlowPane.getChildren().add(new Label(myCommand.getText()));
+		        myScrollPane.setContent(myFlowPane);
+		        myCommand.clear();
+		     //   vbox2.getChildren().add(new Circle(40,40,40,Color.WHEAT));
+		    }
+		});
+	//   EventHandler<ActionEvent> onClick = new EventHandler<ActionEvent>();
+	   
+	   
+	//   buttonProjected.addEventHandler(ActionEvent click, EventHandler onClick);
+	   hbox.getChildren().add(buttonProjected);
 
 	    return hbox;
 	}
@@ -106,7 +134,7 @@ public class GUI extends Application {
 	  //  hbox.setPrefHeight();
 	    vbox.setPadding(new Insets(15, 12, 15, 12));
 	    vbox.setSpacing(10);
-	    vbox.setStyle("-fx-background-color: #333399;");
+	   // vbox.setStyle("-fx-background-color: #333399;");
 
 	    Button buttonCurrent = new Button(s);
 	    buttonCurrent.setPrefSize(100, 20);
@@ -119,15 +147,28 @@ public class GUI extends Application {
 	public void start(Stage stage) throws Exception {
 		myStage = new Stage();
 		myRoot = new Group();
+		myConsoleHistory = new ConsoleHistory();
+		myScrollPane = new ScrollPane();
+		myFlowPane = new FlowPane(Orientation.VERTICAL);
+		//myFlowPane.
+		//myFlowPane.set
 		Scene s = initiate(myRoot);
 		BorderPane pane = setBorderPane(myRoot);
 		VBox vcenter = addVBox("TurtleView");
 		vcenter.setPrefWidth(50);
 		VBox vbox = addVBox("Variables");
-		VBox vbox2 = addVBox("Command History");
+		vbox2 = addVBox("Command History");
+		vbox2.getChildren().addAll(myConsoleHistory.giveLabels());
+		vbox2.getChildren().add(myScrollPane);
 		vcenter.setStyle("-fx-background-color: #FFFFFF;");
 		pane.setLeft(vbox);
 		pane.setRight(vbox2);
+		
+		
+		
+		
+		
+		
 		pane.setCenter(vcenter);
 		myStage.setScene(s);
 		myStage.show();
--- src/textOutput/ConsoleHistory.java
+++ src/textOutput/ConsoleHistory.java
@@ -1,19 +1,38 @@
 package textOutput;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javafx.scene.control.Label;
+import sun.misc.Queue;
+
 
 public class ConsoleHistory extends TextDisplayFactory{
 	
 	/**
 	 * Initializes the console text display that will display console history
 	 */
+	List<String> myCommands;
+	List<Label> myLabels;
 	public ConsoleHistory(){
-		
+		myCommands = new ArrayList<String>();
+		myLabels = new ArrayList<Label>();
 	}
 
 	@Override
 	public void update() {
 		// TODO Auto-generated method stub
 		
+	}
+	public void update(String s){
+		myCommands.add(s);
+	}
+	public List<Label> giveLabels(){
+		//myLabels = new ArrayList<Label>();
+		for(String s:myCommands){
+			myLabels.add(new Label(s));
+		}
+		return myLabels;
 	}
 
 }
