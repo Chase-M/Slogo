82cd8ffa0d298ea28783820898be6261850cce1d
Chase Malik <chase.malik@duke.edu>
2014-10-09 22:30:17 -0400

    Merge backend with front

--- src/parser/TestModel.java
+++ src/parser/TestModel.java
@@ -1,8 +1,9 @@
 package parser;
 
-import java.lang.reflect.InvocationTargetException;
 import java.util.List;
+
 import org.junit.Test;
+
 import command.IntCommand;
 import command.LessCommand;
 import command.SumCommand;
@@ -18,16 +19,9 @@ public class TestModel {
 	private final static double PRECISION = 0.0000000001;
 	/**
 	 * Test to make sure it moves within the model properly
-	 * @throws SecurityException 
-	 * @throws NoSuchMethodException 
-	 * @throws InvocationTargetException 
-	 * @throws IllegalArgumentException 
-	 * @throws IllegalAccessException 
-	 * @throws InstantiationException 
-	 * @throws ClassNotFoundException 
 	 */
 	@Test
-	public void testPosition() throws ClassNotFoundException, InstantiationException, IllegalAccessException, IllegalArgumentException, InvocationTargetException, NoSuchMethodException, SecurityException{
+	public void testPosition(){
 		Parser parser=new Parser();
 		View view = new View();
 		//Actor t=new Turtle(view);
@@ -37,19 +31,12 @@ public class TestModel {
 	}
 	/**
 	 * Test to make sure it throws exceptions properly
-	 * @throws SecurityException 
-	 * @throws NoSuchMethodException 
-	 * @throws InvocationTargetException 
-	 * @throws IllegalArgumentException 
-	 * @throws IllegalAccessException 
-	 * @throws InstantiationException 
-	 * @throws ClassNotFoundException 
 	 */
 	@Test(expected=Exception.class)	
-	public void testParseException() throws ClassNotFoundException, InstantiationException, IllegalAccessException, IllegalArgumentException, InvocationTargetException, NoSuchMethodException, SecurityException{
+	public void testParseException(){
 		Parser parser = new Parser();
 		parser.parse("asjklfhas;hgoask’dhfaga");
-	}
+	}		
 
 	@Test
 	public void testSumCommand(){
