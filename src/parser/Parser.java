7c9ab419f6d6837d2aca92f327c8017d66e9b394
timeshp <timeshp@gmail.com>
2014-10-09 21:05:19 -0400

    made parser methods

--- src/parser/Parser.java
+++ src/parser/Parser.java
@@ -1,16 +1,46 @@
 package parser;
 
+import java.lang.reflect.InvocationTargetException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ResourceBundle;
+import command.Command;
 import javafx.scene.input.KeyCode;
 
 public class Parser {
+    private List<Node> myTreeHeads;
+    private int myIndex;
+    private ResourceBundle myLanguage;
+    private static final String RESOURCE_BUNDLE="resources.languages";
+    private static final String DEFAULT_LANGUAGE="English";
+    private static final String COMMAND_PATH="command.";
+    public Parser(){
+        myTreeHeads=new ArrayList<Node>();
+        setLanguage(DEFAULT_LANGUAGE);
+        
+    }
     /**
      * takes in a string and parses it by puts all commands into a graph 
      * of Nodes 
      * @param string
      * @return the string used if the commands were successful 
+     * @throws SecurityException 
+     * @throws NoSuchMethodException 
+     * @throws InvocationTargetException 
+     * @throws IllegalArgumentException 
+     * @throws IllegalAccessException 
+     * @throws InstantiationException 
+     * @throws ClassNotFoundException 
      */
-   public String parse(String string){
-       return string;
+   public List<Node> parse(String string) throws ClassNotFoundException, InstantiationException, IllegalAccessException, IllegalArgumentException, InvocationTargetException, NoSuchMethodException, SecurityException{
+       myTreeHeads.clear();
+       string=string.replaceAll("[\\\t|\\\n|\\\r]"," ");
+       String[] commands=string.split("\\s+");
+       myIndex=0;
+       while(myIndex<commands.length){
+           myTreeHeads.add(makeTree(commands));
+       }
+       return myTreeHeads;
    }
    /**
     * If user needs to give mouseinput to parser 
@@ -33,5 +63,24 @@ public class Parser {
     */
    public void changeLanguage(String language){
        
+   }
+   private Node makeTree(String[] s) throws ClassNotFoundException, InstantiationException, IllegalAccessException, IllegalArgumentException, InvocationTargetException, NoSuchMethodException, SecurityException{
+       Node node=new Node(makeCommand(s[myIndex]));
+       for(int i=0; i<node.getCommand().getNumInputs(); i++){
+           myIndex++;
+           node.addChild(makeTree(s));
+       }
+       return node;
+   }
+   private Command makeCommand(String command) throws ClassNotFoundException, InstantiationException, IllegalAccessException, IllegalArgumentException, InvocationTargetException, NoSuchMethodException, SecurityException{
+       
+       String className=COMMAND_PATH+command;
+       Class<?> classType=Class.forName(className);
+       Command c=(Command)classType.getConstructor().newInstance();
+       return c;
+               
+   }
+   public void setLanguage(String language){
+       myLanguage=ResourceBundle.getBundle(RESOURCE_BUNDLE+language);
    }
 }
