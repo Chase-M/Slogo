8b4b8d056669fb25ee2f6a2a369519ecb00ebd28
Chase Malik <chase.malik@duke.edu>
2014-10-22 02:50:32 -0400

    Add Boolean Operations

---
+++ src/command/NotCommand.java
@@ -0,0 +1,20 @@
+package command;
+
+import java.util.List;
+
+import parser.Node;
+import workspace.Workspace;
+
+public class NotCommand extends Command {
+
+	public NotCommand(String s){
+		super(s,1);
+	}
+	
+	@Override
+	public double execute(List<Node> inputs, Workspace workspace) {
+		return (inputs.get(0).evaluate(workspace)==0)? 1:0;
+	}
+
+
+}
