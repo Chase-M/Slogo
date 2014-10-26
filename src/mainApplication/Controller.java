24b365eb0b04f5b45ca87472c271ade6977ebcff
timeshp <timeshp@gmail.com>
2014-10-26 01:01:08 -0400

    added get to controller

--- src/mainApplication/Controller.java
+++ src/mainApplication/Controller.java
@@ -6,73 +6,77 @@ import java.util.List;
 import java.util.Map;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+import parser.CommandObject;
 import parser.Node;
 import parser.Parser;
 import workspace.Workspace;
 
+
 public class Controller {
-	
-	private List<Workspace> myWorkspaces;
-	private Parser myParser;
-	private int myActive;
-	private Workspace myActiveWS;
-	//TEMPORARY, REMOVE STAGE
-	public Stage myStage;
-	
-	public Controller(Stage s){//TEMPORARY
-		myStage = s;
-		myWorkspaces = new ArrayList<>();
 
-		myParser = new Parser();
-	}
-	
-	public void parseAndEvaluate(String s) throws Exception{
-		List<Node> list = myParser.parse(s);
-		myActiveWS.evaluate(list);
-	}
-	
-	public int createWorkspace(GUI gui){
-		int i = 0;
-		if(myWorkspaces != null)
-			i = myWorkspaces.size();
-		myActiveWS=new Workspace(i);
-		myActiveWS.addObserver(gui);
-		myActiveWS.createTurtle(0);
+    private List<Workspace> myWorkspaces;
+    private Parser myParser;
+    private int myActive;
+    private Workspace myActiveWS;
+    // TEMPORARY, REMOVE STAGE
+    public Stage myStage;
 
-		myWorkspaces.add(myActiveWS);
-		
-		return myWorkspaces.size()-1;
-	}
-	
-	public void setActive(int id){
-		myActive = id;
-		myActiveWS = myWorkspaces.get(myActive);
-	}
-	
-	public Map<String,Double> getVariables(){
-		return myActiveWS.getVariables();
-	}
-	
-	public Map<Integer, Color> getColors(){
-	    System.out.println(myActiveWS.getColors().size());
-		return myActiveWS.getColors();
-	}
-	public void clear(){
-		myActiveWS.clear();
-	}
-	
+    public Controller (Stage s) {// TEMPORARY
+        myStage = s;
+        myWorkspaces = new ArrayList<>();
 
-	
-	public void saveMem(File f){
-	    try {
-       
+        myParser = new Parser();
+    }
+
+    public void parseAndEvaluate (String s) throws Exception {
+        List<Node> list = myParser.parse(s);
+        myActiveWS.evaluate(list);
+    }
+
+    public int createWorkspace (GUI gui) {
+        int i = 0;
+        if (myWorkspaces != null)
+            i = myWorkspaces.size();
+        myActiveWS = new Workspace(i);
+        myActiveWS.addObserver(gui);
+        myActiveWS.createTurtle(0);
+
+        myWorkspaces.add(myActiveWS);
+
+        return myWorkspaces.size() - 1;
+    }
+
+    public void setActive (int id) {
+        myActive = id;
+        myActiveWS = myWorkspaces.get(myActive);
+    }
+
+    public Map<String, Double> getVariables () {
+        return myActiveWS.getVariables();
+    }
+
+    public Map<String, CommandObject> getCommands () {
+        return myActiveWS.getCommands();
+    }
+
+    public Map<Integer, Color> getColors () {
+        System.out.println(myActiveWS.getColors().size());
+        return myActiveWS.getColors();
+    }
+
+    public void clear () {
+        myActiveWS.clear();
+    }
+
+    public void saveMem (File f) {
+        try {
+
             myActiveWS.writeMem(f);
         }
         catch (Exception e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
         }
-	}
-	
-	
+    }
+
 }
